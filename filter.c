6f92997dda618f536799bff524b819a66344e763
Disi A <adis@live.cn>
2018-08-08 20:47:37 -0400

    Bug fixes.

--- filter.c
+++ filter.c
@@ -24,7 +24,7 @@ BWLowPass* create_bw_low_pass_filter(int order, double sampling_frequency, int h
     for(i=0; i < filter -> n; ++i){
         r = sin(M_PI*(2.0*i+1.0)/(4.0*filter -> n));
         s = a2 + 2.0*a*r + 1.0;
-        filter -> A[i] = 1.0/s;
+        filter -> A[i] = a2/s;
         filter -> d1[i] = 2.0*(1-a2)/s;
         filter -> d2[i] = -(a2 - 2.0*a*r + 1.0)/s;
     }
@@ -46,11 +46,11 @@ BWHighPass* create_bw_high_pass_filter(int order, double sampling_frequency, int
     double r;
     
     int i;
-    
+
     for(i=0; i < filter -> n; ++i){
         r = sin(M_PI*(2.0*i+1.0)/(4.0*filter -> n));
         s = a2 + 2.0*a*r + 1.0;
-        filter -> A[i] = a2/s;
+        filter -> A[i] = 1.0/s;
         filter -> d1[i] = 2.0*(1-a2)/s;
         filter -> d2[i] = -(a2 - 2.0*a*r + 1.0)/s;
     }
@@ -63,11 +63,20 @@ BWBandStop* create_bw_band_stop_filter(int order, double sampling_frequency, int
     return NULL;
 }
 
-void free_bw_low_pass(BWLowPass* filter){
+void free_bw_lphp(BWLowPass* filter){
+    free(filter -> d1);
+    free(filter -> d2);
+    free(filter -> w0);
+    free(filter -> w1);
+    free(filter -> w2);
+    free(filter);
+}
 
+void free_bw_low_pass(BWLowPass* filter){
+    free_bw_lphp(filter);
 }
 void free_bw_high_pass(BWHighPass* filter){
-
+    free_bw_lphp(filter);
 }
 void free_bw_band_pass(BWBandPass* filter){
 
@@ -80,14 +89,21 @@ double low_pass(BWLowPass* filter, double x){
     int i;
     for(i=0; i<filter->n; ++i){
         filter->w0[i] = filter->d1[i]*filter->w1[i] + filter->d2[i]*filter->w2[i] + x;
-        x = filter->A[i]*(filter->w0[i] - 2.0*filter->w1[i] + filter->w2[i]);
+        x = filter->A[i]*(filter->w0[i] + 2.0*filter->w1[i] + filter->w2[i]);
         filter->w2[i] = filter->w1[i];
         filter->w1[i] = filter->w0[i];
     }
     return x;
 }
-double high_pass(BWHighPass* filter, double input){
-
+double high_pass(BWHighPass* filter, double x){
+    int i;
+    for(i=0; i<filter->n; ++i){
+        filter->w0[i] = filter->d1[i]*filter->w1[i] + filter->d2[i]*filter->w2[i] + x;
+        x = filter->A[i]*(filter->w0[i] - 2.0*filter->w1[i] + filter->w2[i]);
+        filter->w2[i] = filter->w1[i];
+        filter->w1[i] = filter->w0[i];
+    }
+    return x;
 }
 double band_pass(BWBandPass* filter, double input){
 
